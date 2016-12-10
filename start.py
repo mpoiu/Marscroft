43c9aa6084a601122e8b323f70495c0885ca4b72
piotr <pjacek@mailplus.pl>
2016-12-10 18:13:40 +0100

    Add main window class

--- start.py
+++ start.py
@@ -1,8 +1,14 @@
 import sys
 from PyQt4.QtGui import *
 
+class MyMainWindow(QMainWindow):
+    def __init__(self, parent = None):
+        super(MyMainWindow, self).__init__(parent)
+        self.label = QLabel("Hello from Mars", self)
+        self.setCentralWidget(self.label)
+
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    mainwindow=QMainWindow()
+    mainwindow = MyMainWindow()
     mainwindow.show()
     app.exec_()
